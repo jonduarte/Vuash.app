fa05778a1daf6911787c2ef1fc71f52d472562a0
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2017-05-24 19:00:07 +0100

    Initial Commit

---
+++ Vuash/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Vuash
+//
+//  Created by Jonathan Duarte on 24/05/2017.
+//  Copyright © 2017 Jonathan Duarte. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
